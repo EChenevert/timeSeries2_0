0b114884988b732d8ab0c348c68fad1c28708f63
Chenevert, Etienne A <etachen@iu.edu>
2022-10-21 11:45:47 -0400

    trying to figure out what I did

--- bayesAnalysis.py
+++ bayesAnalysis.py
@@ -24,12 +24,16 @@ df = pd.read_csv(r"D:\Etienne\fall2022\CRMS_data\bayes2year\CRMS_dfi.csv", encod
 outcome = 'Accretion Rate (mm/yr)'
 df_vars = df.columns.values
 
-
+df['distance_to_river_m'] = np.log(df['distance_to_river_m'])
+# df['Tide_Amp (ft)'] = np.log(df['Tide_Amp (ft)'])
+df = df.drop('Tide_Amp (ft)', axis=1)
 # Normality test
 
 
 # Normalize dataset between 0 and 1
-x_scaler = MinMaxScaler()
+x_scaler = MinMaxScaler((0, 1))
+# x_scaler = MinMaxScaler((-1, 1))
+
 phi = x_scaler.fit_transform(df.drop(outcome, axis=1))
 
 # Shuffle the dataset to avoid training in only certain spatial areas
@@ -60,13 +64,13 @@ for frac in trainFracArr:
     for i in range(100):
         # Train test split
         X_train, X_test, y_train, y_test = train_test_split(phi[:, 1:], t, train_size=frac)  # 0 cuz 0 corresponds to the RSLR var
-        B, a, eff_lambda_winfo, itr = bml.iterative_prog_wPrior(X_train, y_train,
-                                                          phi[:, 0])   # 0 cuz 0 corresponds to the RSLR var
+        # B, a, eff_lambda_winfo, itr = bml.iterative_prog_wPrior(X_train, y_train,
+        #                                                   phi[:, 0])   # 0 cuz 0 corresponds to the RSLR var
         B, a, eff_lambda, itr = bml.iterative_prog(X_train, y_train)  # std of 0.5 cuz i normalize variables between 0 and 1
 
-        var_weights_map_winfo = bml.leastSquares(eff_lambda_winfo, X_train, y_train)
-        map_MSE_winfo = bml.returnMSE(X_test, var_weights_map_winfo, y_test)
-        hold_mapMSE_winfo.append(map_MSE_winfo)
+        # var_weights_map_winfo = bml.leastSquares(eff_lambda_winfo, X_train, y_train)
+        # map_MSE_winfo = bml.returnMSE(X_test, var_weights_map_winfo, y_test)
+        # hold_mapMSE_winfo.append(map_MSE_winfo)
 
         var_weights_map = bml.leastSquares(eff_lambda, X_train, y_train)
         map_MSE = bml.returnMSE(X_test, var_weights_map, y_test)
@@ -79,12 +83,12 @@ for frac in trainFracArr:
     trainSize.append(frac)
     MSE_ml_ls.append(np.mean(hold_mlMSE))
     MSE_map_ls.append(np.mean(hold_mapMSE))
-    MSE_map_winfo.append(np.mean(hold_mapMSE_winfo))
+    # MSE_map_winfo.append(np.mean(hold_mapMSE_winfo))
 
 plt.figure()
 plt.plot(trainSize, MSE_map_ls, label='MAP')
 plt.plot(trainSize, MSE_ml_ls, label='MLE')
-plt.plot(trainSize, MSE_map_winfo, label='informed MAP')
+# plt.plot(trainSize, MSE_map_winfo, label='informed MAP')
 # plt.ylim(0, 500)
 plt.title('MSE versus Train Size')
 plt.ylabel('MSE')
